b8e3aa1bb51ae1d2cc664cac149ddbcc86808dbc
Sargunan Gurumurthy <sargunan1213@gmail.com>
2024-10-05 17:25:44 -0400

    Cleaning up

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl font-pblack">Hi</Text>
+    </View>
+  );
+}
